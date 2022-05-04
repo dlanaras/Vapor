eda66713aace3d2767d455a94354fc6e4cfaf823
Sorry4Nothing <robnoleh@gmail.com>
2022-05-04 20:26:21 +0200

    final commit

--- views/signup/reset.php
+++ views/signup/reset.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/style.css">
     <title>Vapor</title>
 </head>
-<body>
+<body style="padding-top: 340px; padding-left: 40%">
     <h1>Reset Password</h1>
     <?php
     require_once("../../classes/SessionManager.php");
