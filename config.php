f454f77e429076e9ddc9951e0a9af913753bbe13
Sorry4Nothing <robnoleh@gmail.com>
2022-05-05 10:40:32 +0200

    added error page

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     "host" => "localhost",
-    "name" => "DB_Vapor",
-    "username" => "pma",
-    "pass" => "pmapass"
+    "name" => "db_vapor",
+    "username" => "root",
+    "pass" => "root"
 )
 ?>
