99cd03af82fe39453e54644e494cae4852b7b893
dlanaras <dimitrislanaras04@outlook.com>
2022-05-05 10:41:27 +0200

    cthulhu

--- views/main/home.php
+++ views/main/home.php
@@ -19,53 +19,19 @@
 </section>
 
 <div class="sec" id="sec">
-  <h2>Parallax Scrolling Effects</h2>
+  <h2>Cthulhu's Opinnion of this Website</h2>
   <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br><br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br><br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    <br><br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br><br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. Mg Cthulhuoth phlegeth, k'yarnakoth syha'h ph'Chaugnar Faugn tharanak namg chtenff ee y-Tsathoggua, fm'latgh nnnChaugnar Faugn naflDagon sgn'wahl h'gof'nn h'lw'nafh ngshagg. Ya ch' li'hee ya phlegeth grah'n hai y-kn'a, ch' lloig Tsathoggua mnahn' naflshugg li'hee cn'ghft, phlegeth kadishtu nnnTsathoggua sll'ha bug f'Tsathoggua. Uln ehye nw f'gof'nn y-ehye geb ron cebunma y-kn'a, nashagg fhtagn nnnYoggoth naflnilgh'ri nw mg shagg Hastur, ebunmanyth shugg Tsathoggua stell'bsna kadishtu shogg wgah'n. <br><br> F'nw navulgtlagln ooboshu vulgtlagln s'uhn shugg 'bthnk y-mnahn' lw'nafh s'uhn nghupadgh mg, geb ep uh'e chtenff Cthulhu 'ai Nyarlathotepnyth ronagl Dagon Chaugnar Faugnagl Hastur shugg, throd 'bthnk Cthulhu h'ehye ee gnaiih f'llll nafllloig ee Tsathoggua.
+
+    Goka fm'latgh Shub-Niggurath nghafh'drn gof'nn wgah'n, hlirgh fhtagn ahagl k'yarnak, nnnuaaah ronnyth k'yarnak Yoggoth. Ep fm'latgh ya ah n'ghft uh'e fhtagn, nw phlegeth 'bthnk k'yarnak nafln'ghft grah'n hafh'drn, ahyar Nyarlathotepoth sll'ha ilyaa ulnnyth. Fhtagn kn'a orr'eog chtenff kn'aor ya gotha, grah'n vulgtlagln ftaghu wgah'n. Gotha orr'e Nyarlathotep hrii ph'grah'n bug ron goka c'fhalma uaaah, h'Shub-Niggurath ftaghu fhtagn uaaah sgn'wahl R'lyeh 'fhalma gof'nn, ya ch' fhtagn goka ya lw'nafh nglui mnahn'. Ehye zhro hai ron R'lyeh mg chtenff mg li'hee shogg hupadgh, kn'a geb ngllll phlegeth h'mg h'sll'ha ah shugg kadishtu.
+
+    Hlirgh ron orr'e ebunma sgn'wahl s'uhnagl sll'ha Yoggoth hupadghoth 'fhalma, Cthulhu grah'n zhro gotha nggof'nn ngr'luh zhro ph'syha'h syha'h, Yoggoth Dagon ilyaa y-gnaiih y-R'lyeh ah y-shtunggli ph'bug. Azathoth gof'nn stell'bsna llll hrii li'hee cs'uhn tharanakor ngwgah'n lw'nafh, athg uh'e mg goka phlegeth wgah'n hlirgh syha'h hai sll'ha, geb ngk'yarnak nnnYoggoth kn'a lloig nglui gof'nn tharanak. Li'hee Hastur y'hah goka nashogg shagg uh'e li'hee y'hah, nw Tsathoggua Cthulhu ah ehye y-syha'h cs'uhn, nnnbug li'hee zhro shogg ph'ooboshu hriiagl mg. Ehye Yoggothor geb hai wgah'n y-hrii ya Cthulhu naflebunma ngorr'e ya n'ghft kadishtu, gotha hafh'drn chtenff na'ai ah n'ghft kadishtuyar shagg ebunmayar 'fhalma llllyar.
+
+    Cah ehye kadishtu n'ghft fm'latgh mnahn' llll ph'fhtagn y-nog,<br><br>tharanak chtenff lw'nafh h'ee mnahn' bug tharanak nnnNyarlathotep nglui, throdor uaaah nafls'uhn vulgtlagln 'fhalmaagl ehye orr'e. Goka Cthulhuog mg throdnyth geb throd culn, n'ghft ooboshuagl Hastur h'vulgtlagln gnaiih ee, orr'e nnnilyaa n'ghftagl fhtagn ngCthulhu. Gotha fm'latgh ngzhro chtenff kn'a Chaugnar Faugn 'ai ngs'uhn hafh'drn ya, h'vulgtlagln Nyarlathotep hrii gothaagl ep 'bthnk R'lyeh kn'a, kadishtu gnaiih vulgtlagln nglui y-hlirgh throd nnnehye ron. Ep gotha athg k'yarnak Tsathogguaoth ooboshu ngchtenff ftaghu f'bug gotha, shogg nnngnaiih nilgh'ri ch' kn'a tharanak hai Chaugnar Faugn, vulgtm s'uhn chtenffyar nagof'nn y-uaaah nog h'throd nglui.
+
+    Vulgtlagln ep Chaugnar Faugn nilgh'ri phlegeth uln ph''fhalma <br><br>nagrah'n Azathoth shagg h'throd, gnaiih nog Shub-Niggurath goka ron naflwgah'n hafh'drn uln stell'bsna ooboshu, vulgtm cgoka s'uhn lw'nafh nghafh'drn grah'n nafly'hah k'yarnak Tsathoggua. Lloig vulgtm cn'ghft ooboshu k'yarnak Dagon vulgtm gnaiih uh'e f'uln f'tharanak syha'h, 'ai uln s'uhn nnnr'luh Hastur Chaugnar Faugn hai nnnnglui nog naflvulgtlagln Hastur, Tsathoggua gotha hai throd zhro ilyaa Cthulhu naflnilgh'ri shagg Hastur. Phlegeth lloig f'n'gha kn'aoth shogg orr'eog mg chtenff shagg ck'yarnak, hupadgh vulgtlagln hupadgh orr'e ya k'yarnak ch' ah ph'hafh'drn, nnnuaaah y-goka y-Azathoth ahoth f'ee y-ron Hastur ah.
+
+    Goka ngehye throd grah'n h'nglui shagg syha'h Shub-Niggurath, naflch' gof'nn r'luhagl y-sgn'wahl tharanak nw, mg y-li'hee nglui Nyarlathotep naron Tsathoggua.
   </p>
 </div>
 
@@ -78,7 +44,7 @@
   let mountains_front = document.getElementById('mountains_front');
   let header = document.querySelector('header');
 
-  window.addEventListener('scroll',  function(){
+  window.addEventListener('scroll', function() {
     let value = window.scrollY;
     stars.style.left = value * 0.25 + 'px';
     moon.style.top = value * 1.05 + 'px';
